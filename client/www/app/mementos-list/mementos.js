e7b765578d90610f9fef1f4d144134d772e0b529
Kiran Rao <k.ranrao@gmail.com>
2015-02-27 13:23:03 -0800

    Refactors mementos controller to account for whether user is creating a moment or just reviewing their list

--- client/www/app/mementos-list/mementos.js
+++ client/www/app/mementos-list/mementos.js
@@ -6,20 +6,27 @@
     .controller('Mementos', Mementos);
 
   /* @ngInject */
-  function Mementos(dataservice, CurrentMoment) {
+  function Mementos(dataservice, CurrentMoment, $state) {
     /*jshint validthis: true */
     var vm = this;
     vm.mementos = {};
     vm.title = 'Mementos';
     vm.addMoment = addMoment;
+    vm.getMoment = getMoment;
+    vm.getMementos = getMementos
+    vm.goToMomentCreate = goToMomentCreate;
+    vm.createMode = false;
+    vm.momentID = {};
 
     activate();
 
     ////////////////////////////////////////////////////////////
 
     function activate() {
-      return getMementos().then(function() {
+      return vm.getMementos().then(function() {
         console.log('Activated mementos view');
+        // get moment and check if we are in createMode
+        return getMoment();
       });
     }
 
@@ -35,19 +42,38 @@
     }
 
     function addMoment(mementoID) {
-      var momentID = CurrentMoment.get();
+      // if we are in momentCreate mode, addMoment
+      if(vm.createMode) {
+        // update memento in database
+        return dataservice.updateMemento(mementoID, vm.momentID)
+          .then(function(data) {
+            console.log('Memento ' + mementoID + ' has been updated');
+            
+            // NOTE: sets moment back to an empty object
+            CurrentMoment.set({});
+            
+            // NOTE: sets moment create mode back to false
+            vm.createMode = false;
+          })
+          .catch(function(err) {
+            console.error('There was an error updating the memento:', err);
+          })
+      }
 
-      // update memento in database
-      return dataservice.updateMemento(mementoID, momentID)
-        .then(function(data) {
-          console.log('Memento ' + mementoID + ' has been updated');
-          
-          // NOTE: sets moment back to an empty object
-          CurrentMoment.set({});
-        })
-        .catch(function(err) {
-          console.error('There was an error updating the memento:', err);
-        })
+    }
+
+    function getMoment() {
+      vm.momentID = CurrentMoment.get();
+      
+      // check if we are in create mode
+      if(vm.momentID.hasOwnProperty('momentID')) {
+        vm.createMode = true;
+      } 
+    }
+
+    // NOTE: all this nav functionality are candidates for a nav service 
+    function goToMomentCreate () {
+      return $state.go('moment')
     }
 
   }
